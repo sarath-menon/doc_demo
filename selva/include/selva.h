e797fbba59354f0106150530f204b1c442b91269
smenon <smenon@ethz.ch>
2021-12-03 02:30:50 +0530

    breathe works now

--- selva/include/selva.h
+++ selva/include/selva.h
@@ -9,7 +9,6 @@ public:
     \param a an integer argument.
     \param s a constant character pointer.
     \return The test results
-    \sa QTstyle_Test(), ~QTstyle_Test(), testMeToo() and publicVar()
   */
   void disp1();
 };
